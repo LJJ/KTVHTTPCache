65298b701ace0e9b592ab0a551471093e8657398
LJJ <lujiji@liblib.ai>
2025-10-28 14:41:31 +0800

    SPM: use internal umbrella header inside include/ to avoid path escape; fix module import in Xcode

---
+++ KTVHTTPCache/include/KTVHTTPCache/KTVHTTPCache.h
@@ -0,0 +1,11 @@
+// Umbrella header for SwiftPM module
+#import "KTVHCError.h"
+#import "KTVHCRange.h"
+#import "KTVHCDataReader.h"
+#import "KTVHCDataLoader.h"
+#import "KTVHCDataHLSLoader.h"
+#import "KTVHCDataRequest.h"
+#import "KTVHCDataResponse.h"
+#import "KTVHCDataCacheItem.h"
+#import "KTVHCDataCacheItemZone.h"
+
